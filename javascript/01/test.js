6ce98914734e65dee34ea6f4affa665b60be6b40
RohJaeseung <sola4441@gmail.com>
2021-09-03 19:01:56 +0900

    0903

---
+++ javascript/01/test.js
@@ -0,0 +1,12 @@
+var spanStrA = "fdlfjlksdjfkl";
+var spanStrB = "zzxxxxxxxxzzzz"
+
+var str = "<div>";
+    str += "<header>";
+    str += "<nav>";
+    str += "<span>" + spanStrA + "</span><span>" + spanStrB + "</span>"
+    str += "</nav>";
+    str += "</header>";
+    str += "</div>"
+
+document.write(str)
